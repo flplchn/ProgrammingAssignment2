ce1c7b3a0fa24bd74603a380b15f193a1e86a8e0
Filippo Lecchini <flplchn@Filippos-MacBook-Pro.local>
2018-03-11 15:41:33 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions generate an inverse matrix for every square and invertible matrix that can be stored
+## from the local environment where the matrix is computed to the global environment where it can
+## be recalled
 
-## Write a short comment describing this function
+## The first matrix doesn't compute the actual inversion but initializes the variables instead
+## and the functions utilized in the next function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv1 <- NULL
+  set <- function(y) {
+    x <<- y
+    inv1 <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invr) inv1 <<- invr
+  getinv <- function() inv1
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function inverts the matrix and caches it for the global environment
 
 cacheSolve <- function(x, ...) {
+  inv1 <-x$getinv()
+  if(!is.null(inv1)) {
+    message("getting cached data")
+    return(inv1)
+  }
+  data <- x$get()
+  inv1 <- solve(data,)
+  x$setinv(inv1)
+  inv1
         ## Return a matrix that is the inverse of 'x'
 }
